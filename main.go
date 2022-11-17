d7cab6195e4c5cd921e6233a232cac61b2e668d6
Timo Ischen <info@timo-ischen.de>
2022-11-17 10:30:54 +0100

    feat: Add gracefully shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/story-pointify/room-service/ws"
 )
@@ -14,5 +20,33 @@ func main() {
 		ws.ServeWs(wsServer, w, r)
 	})
 
-	http.ListenAndServe(":1337", nil)
+	srv := &http.Server{
+		Addr:    ":1337",
+		Handler: nil,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("Server started 🚀")
+
+	<-done
+	log.Println("Server stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("💩 Server Shutdown Failed:%v+", err)
+	}
+
+	log.Println("Server exited properly 😴")
 }
